427fb9fed5cf2ec15ce2cd253068dbfc5c3fde1a
audyningrum <audynurul@gmail.com>
2022-03-29 12:07:18 +0700

    Membuat tampilan

--- app/src/main/java/org/d3if2013/learntocount/MainActivity.kt
+++ app/src/main/java/org/d3if2013/learntocount/MainActivity.kt
@@ -2,10 +2,18 @@ package org.d3if2013.learntocount
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import org.d3if2013.learntocount.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
