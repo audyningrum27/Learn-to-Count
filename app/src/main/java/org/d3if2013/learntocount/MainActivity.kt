3fc6ac2bac6027ff622f5cdade2ee5c8f2b00774
audyningrum <audynurul@gmail.com>
2022-03-30 22:53:18 +0700

    Membuat perhitungan dan tambah icon

--- app/src/main/java/org/d3if2013/learntocount/MainActivity.kt
+++ app/src/main/java/org/d3if2013/learntocount/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.submit.setOnClickListener { submitClick() }
+        binding.reset.setOnClickListener { clearText() }
     }
 
     private fun submitClick() {
@@ -57,5 +58,125 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.hanya_angka, Toast.LENGTH_LONG).show()
             return
         }
+
+        var marks = 0
+        var tanda = ""
+        val benar = "Benar"
+        val salah = "Salah"
+        var nilai = 0
+
+        val rightAnswer1 = "48"
+        val rightAnswer2 = soal2 == R.id.soal2_pil3
+        val ra2 = getSoal2(rightAnswer2)
+        val rightAnswer3 = "39"
+        val rightAnswer4 = soal4 == R.id.soal4_pil1
+        val ra4 = getSoal4(rightAnswer4)
+        val rightAnswer5 = "100"
+
+        if (soal1 == rightAnswer1) {
+            marks++
+            binding.no1TextView.text = getString(R.string.one, soal1, benar)
+        } else {
+            binding.no1TextView.text = getString(R.string.one, soal1, salah)
+        }
+
+        if(rightAnswer2) {
+            marks++
+            binding.no2TextView.text = getString(R.string.two, ra2, benar)
+        } else {
+            binding.no2TextView.text = getString(R.string.two, ra2, salah)
+        }
+
+        if (soal3 == rightAnswer3) {
+            marks++
+            binding.no3TextView.text = getString(R.string.three, soal3, benar)
+        } else {
+            binding.no3TextView.text = getString(R.string.three, soal3, salah)
+        }
+
+        if(rightAnswer4) {
+            marks++
+            binding.no4TextView.text = getString(R.string.four, ra4, benar)
+        } else{
+            binding.no4TextView.text = getString(R.string.four, ra4, salah)
+        }
+
+        if (soal5 == rightAnswer5) {
+            marks++
+            binding.no5TextView.text = getString(R.string.five, soal5, benar)
+        } else {
+            binding.no5TextView.text = getString(R.string.five, soal5, salah)
+        }
+
+        if(marks == 5){
+            tanda = "Benar 5 dari 5 soal"
+            nilai = 100
+            binding.marksTextView.text = getString(R.string.marks, tanda)
+            binding.gradeTextView.text = getString(R.string.grade, nilai)
+        } else if(marks == 4) {
+            tanda = "Benar 4 dari 5 soal"
+            nilai = 80
+            binding.marksTextView.text = getString(R.string.marks, tanda)
+            binding.gradeTextView.text = getString(R.string.grade, nilai)
+        } else if(marks == 3) {
+            tanda = "Benar 3 dari 5 soal"
+            nilai = 60
+            binding.marksTextView.text = getString(R.string.marks, tanda)
+            binding.gradeTextView.text = getString(R.string.grade, nilai)
+        } else if(marks == 2) {
+            tanda = "Benar 2 dari 5 soal"
+            nilai = 40
+            binding.marksTextView.text = getString(R.string.marks, tanda)
+            binding.gradeTextView.text = getString(R.string.grade, nilai)
+        } else if(marks == 1) {
+            tanda = "Benar 1 dari 5 soal"
+            nilai = 20
+            binding.marksTextView.text = getString(R.string.marks, tanda)
+            binding.gradeTextView.text = getString(R.string.grade, nilai)
+        } else {
+            tanda = "Benar 0 dari 5 soal"
+            nilai = 0
+            binding.marksTextView.text = getString(R.string.marks, tanda)
+            binding.gradeTextView.text = getString(R.string.grade, nilai)
+        }
+    }
+
+    private fun getSoal2(rightAnswer2:Boolean): String {
+        val soal2 = binding.soal2RadioGroup.checkedRadioButtonId
+        val stringRes = if(rightAnswer2) {
+            R.string.soal2_pil3
+        } else if (soal2 == R.id.soal2_pil1) {
+            R.string.soal2_pil1
+        } else {
+            R.string.soal2_pil2
+        }
+        return getString(stringRes)
+    }
+
+    private fun getSoal4(rightAnswer4:Boolean): String {
+        val soal4 = binding.soal4RadioGroup.checkedRadioButtonId
+        val stringRes = if(rightAnswer4) {
+            R.string.soal4_pil1
+        } else if (soal4 == R.id.soal4_pil2) {
+            R.string.soal4_pil2
+        } else {
+            R.string.soal4_pil3
+        }
+        return getString(stringRes)
+    }
+
+    private fun clearText() {
+        binding.soal1Inp.setText("")
+        binding.soal2RadioGroup.clearCheck()
+        binding.soal3Inp.setText("")
+        binding.soal4RadioGroup.clearCheck()
+        binding.soal5Inp.setText("")
+        binding.marksTextView.setText("")
+        binding.gradeTextView.setText("")
+        binding.no1TextView.setText("")
+        binding.no2TextView.setText("")
+        binding.no3TextView.setText("")
+        binding.no4TextView.setText("")
+        binding.no5TextView.setText("")
     }
 }
